dffa11b6fd4232dd2e6947bb843c20f886a29e4a
JoseManuel88 <113127826+JoseManuel88@users.noreply.github.com>
2022-11-12 13:44:02 +0100

    commit  finalproyect react Front

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import './Header.scss';
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
+      <h2>Hola que ase!!</h2>
+      <div className='logo'>
+        <p>Recetas</p>
+      </div>
+      <nav className='navbar'>
+        <ul>
+          <li>Recetas caseras </li>
+          <li>Ensaladas</li>
+          <li>Postres</li>
+          <li>Helados</li>
+        </ul>
+        
+        <div className='login'>
+          <p>
+            Login/registro
+          </p>
+        </div>
+        </nav>
+      
         
     </div>
   )
